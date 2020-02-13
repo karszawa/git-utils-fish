c1da006c197c5b964ff3309bee53219d35f34ecc
Hiroaki KARASAWA <karszawa@users.noreply.github.com>
2020-02-13 16:57:46 +0900

    feat: initialize

---
+++ functions/merge-with-search.fish
@@ -0,0 +1,9 @@
+function merge-with-search
+  git branch -a --sort=-authordate |
+    grep -v -e '->' -e '*' |
+    perl -pe 's/^\h+//g' |
+    perl -pe 's#^remotes/origin/###' |
+    perl -nle 'print if !$c{$_}++' |
+    peco |
+    xargs git merge
+end
